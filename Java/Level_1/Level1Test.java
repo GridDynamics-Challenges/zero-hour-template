b162a5610ba7df53535bc16b11315591c645796d
dbrooksGD <dbrooks@griddynamics.com>
2026-04-30 12:09:48 -0500

    Add unit tests for cleanData and findPeak methods

--- Java/Level_1/Level1Test.java
+++ Java/Level_1/Level1Test.java
@@ -1,1 +1,33 @@
+package Level_1;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Level1Test {
+
+    @Test
+    public void testCleanDataStandard() {
+        int[] input = {1200, -50, 3400, -100, 2100};
+        int[] expected = {1200, 3400, 2100};
+        assertArrayEquals(expected, Main.cleanData(input));
+    }
+
+    @Test
+    public void testCleanDataAllNegative() {
+        int[] input = {-500, -200, -100};
+        int[] expected = {};
+        assertArrayEquals(expected, Main.cleanData(input));
+    }
+
+    @Test
+    public void testFindPeakStandard() {
+        int[] input = {1200, 3400, 2100, 5600, 4300};
+        assertEquals(5600, Main.findPeak(input));
+    }
+
+    @Test
+    public void testFindPeakPlateau() {
+        int[] input = {4000, 4000, 2500, 1000};
+        assertEquals(4000, Main.findPeak(input));
+    }
+}
