73bbef09da6bafd3bfa69322275aa00caac9b747
dbrooksGD <dbrooks@griddynamics.com>
2026-04-30 11:56:33 -0500

    Add function stubs for data cleaning and peak finding

--- Python/Level_1/main.py
+++ Python/Level_1/main.py
@@ -1,1 +1,21 @@
+"""
+Operation High Vantage - Level 1
+Target Time: 15 Minutes
+"""
 
+def cleanData(arr):
+    """
+    Takes an array of altitude integers.
+    Returns a new array with all negative numbers removed.
+    """
+    # Write your logic here
+    pass
+
+
+def findPeak(arr):
+    """
+    Takes an array of cleaned altitude integers.
+    Returns the highest altitude.
+    """
+    # Write your logic here
+    pass
