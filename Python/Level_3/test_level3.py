62ce3313fcd141ef255720c8a1d0a676d476ccea
dbrooksGD <dbrooks@griddynamics.com>
2026-04-30 12:48:24 -0500

    Implement tests for findSteepestPath function
    Added unit tests for findSteepestPath function, including standard, tie, and empty matrix cases.

--- Python/Level_3/test_level3.py
+++ Python/Level_3/test_level3.py
@@ -1,1 +1,23 @@
+import pytest
+from main import findSteepestPath
 
+class TestLevel3:
+    
+    def test_findSteepestPath_standard(self):
+        path_a = [1000, -50, 1500, 1200]
+        path_b = [800, 1000, 1500, -90]
+        path_c = [2000, 1000, 500]
+        
+        matrix = [path_a, path_b, path_c]
+        
+        assert findSteepestPath(matrix) == path_b
+        
+    def test_findSteepestPath_tie(self):
+        path_a = [100, 200]
+        path_b = [300, 400]
+        
+        matrix = [path_a, path_b]
+        assert findSteepestPath(matrix) == path_a
+
+    def test_findSteepestPath_empty_matrix(self):
+        assert findSteepestPath([]) == []
