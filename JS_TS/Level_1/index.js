436afde3be0b12c022665bee0da626add3f84013
dbrooksGD <dbrooks@griddynamics.com>
2026-04-30 12:15:11 -0500

    Add cleanData and findPeak function stubs

--- JS_TS/Level_1/index.js
+++ JS_TS/Level_1/index.js
@@ -1,1 +1,26 @@
+/**
+ * Operation High Vantage - Level 1
+ * Target Time: 15 Minutes
+ */
 
+/**
+ * Takes an array of altitude integers.
+ * Returns a new array with all negative numbers removed.
+ */
+function cleanData(arr) {
+    // Write your logic here
+    
+    return [];
+}
+
+/**
+ * Takes an array of cleaned altitude integers.
+ * Returns the highest altitude.
+ */
+function findPeak(arr) {
+    // Write your logic here
+    
+    return 0;
+}
+
+module.exports = { cleanData, findPeak };
